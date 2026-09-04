4f07236711b9028eca1096791df24e9a446c7a24
HusseinO1 <hmkobeid@gmail.com>
2026-09-04 00:00:49 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t)
+    {
+        unordered_map<char, int> m;
+
+        for(auto& c : s)
+        {
+            m[c]++;
+        }
+
+        for(auto c : t)
+        {
+            m[c]--;
+        }
+
+        for(pair<char, int> p : m)
+        {
+            if(p.second != 0)
+                return false;
+        }
+        return true;
+    }
+};
